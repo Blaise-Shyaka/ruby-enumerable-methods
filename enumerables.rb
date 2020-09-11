7890c81c2d464c8a9c01d465441704091e76ec51
alvaro <aaruizf@unal.edu.co>
2020-09-11 16:05:54 -0500

    Fix rubocop errors related to spacing, redundancy and guard clauses

--- enumerables.rb
+++ enumerables.rb
@@ -1,188 +1,218 @@
 module Enumerable
-def my_each()
-  return self unless self.kind_of? Array
-  return to_enum unless block_given?
-  i=0
-  while i<=self.length-1
-    yield(self[i])
-    i+=1
-  end
-  return self
-end
+  def my_each()
+    return self unless self.is_a? Array
 
-def my_each_with_index
-  return to_enum unless block_given?
-  object_to_return = []
-  for i in 0...self.length
-   object_to_return << yield(self[i], i)
-  end
-  return self
-end
+    return to_enum unless block_given?
 
-def my_select
-  return to_enum unless block_given?
-  object_to_return = []
-  if(self.kind_of? Range)
-    self.to_a.my_each {|elt| object_to_return.push(elt) if yield(elt)}
+    i = 0
+    while i <= self.length - 1
+      yield(self[i])
+      i += 1
+    end
+    return self
+  end
+
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    i=0
+    while i < self.length
+      yield(self[i], i)
+      i+=1
+    end
+    return self
+  end
+
+  def my_select
+    return to_enum unless block_given?
+
+    object_to_return = []
+    if(self.is_a? Range)
+      self.to_a.my_each { |elt| object_to_return.push(elt) if yield(elt) }
+      return object_to_return
+    end
+    self.my_each do |element|
+      object_to_return << element if yield(element)
+    end
     return object_to_return
   end
-  self.my_each do |element|
-    object_to_return << element if yield(element)
-  end
-  return object_to_return
-end
 
-def my_all?(*arg, &block)
-  if arg.length == 1
-    if arg[0].kind_of? Class
-      i=0
-      while i < self.length
-        return false unless self[i].kind_of? arg[0]
-        i += 1
-      end 
-      return true
-    else
-      i=0
-      while i < self.length
-        return false if self[i] != arg
-        i += 1
+  def my_all?(*arg, &block)
+    if arg.length == 1
+      i = 0
+      if arg[0].is_a? Class
+        while i < self.length
+          return false unless self[i].is_a? arg[0]
+
+          i += 1
+        end 
+        return true
+      else
+        while i < self.length
+          return false if self[i] != arg
+
+          i += 1
+        end
       end
       return true
     end
-  end
-  block = lambda{|obj| obj} unless block_given?
-  i=0
-  while i < self.length
-    return false unless block.call(self[i])
-    i += 1
-  end
-  return true
-end
+    block = lambda { |obj| obj } unless block_given?
 
-def my_any?(*arg, &block)
-  if arg.length == 1
-    if arg[0].kind_of? Class
-      i=0
-      while i < self.length
-        return true if self[i].kind_of? arg[0]
-        i += 1
-      end 
-      return false
-    else
-      i=0
-      while i < self.length
-        return true if self[i] == arg
-        i += 1
+    i = 0
+    while i < self.length
+      return false unless block.call(self[i])
+
+      i += 1
+    end
+    return true
+  end
+
+  def my_any?(*arg, &block)
+    if arg.length == 1
+      i = 0
+      if arg[0].is_a? Class
+        while i < self.length
+          return true if self[i].is_a? arg[0]
+
+          i += 1
+        end
+        return false
+      else
+        i = 0
+        while i < self.length
+          return true if self[i] == arg
+
+          i += 1
+        end
       end
       return false
     end
-  end
-  block = lambda{|obj| obj} unless block_given?
-  i=0
-  while i < self.length
-    return true if block.call(self[i])
-    i += 1
-  end
-  return false
-end
+    block = lambda { |obj| obj } unless block_given?
 
-def my_none?(*arg, &block)
-  return !my_any?(*arg, &block)
-end
+    i = 0
+    while i < self.length
+      return true if block.call(self[i])
 
-def my_count(*arg, &block)
-  count = 0
-  if arg.length == 1
-      i=0
+      i += 1
+    end
+    return false
+  end
+
+  def my_none?(*arg, &block)
+    return !my_any?(*arg, &block)
+  end
+
+  def my_count(*arg, &block)
+    count = 0
+    if arg.length == 1
+      i = 0
       while i < self.length
         count += 1 if self[i] == arg[0]
         i += 1
       end
       return count
-  end
-  block = lambda{|obj| obj} unless block_given?
-  i=0
-  while i < self.length
-    count += 1 if block.call(self[i])
-    i += 1
-  end
-  return count
-end
+    end
+    block = lambda { |obj| obj } unless block_given?
 
-def my_map
-  res = []
-  if(self.kind_of? Range)
-    self.to_a.my_each {|elt| res.push(yield(elt))}
+    i = 0
+    while i < self.length
+      count += 1 if block.call(self[i])
+      i += 1
+    end
+    return count
+  end
+
+  def my_map
+    res = []
+    if self.is_a? Range
+      self.to_a.my_each { |elt| res.push(yield(elt)) }
+      return res
+    end
+    return self unless self.is_a? Array
+
+    return to_enum unless block_given?
+
+    self.my_each { |elt| res.push(yield(elt)) }
     return res
   end
-  return self unless self.kind_of? Array
-  return to_enum unless block_given?
-  self.my_each {|elt| res.push(yield(elt))}
-  return res
-end
 
-def my_inject (*arg, &block)
-  return raise ArgumentError.new "wrong number of arguments (given #{arg.length}, expected 0..2)" unless arg.length < 3
-  return "Error" unless self.kind_of? Array
-  result=0
-  if !block_given?  
+  def my_inject(*arg)
+    return raise ArgumentError.new "wrong number of arguments (given #{arg.length}, expected 0..2)" unless arg.length < 3
+
+    return 'Error' unless (self.is_a? Array) || (self.is_a? Range)
+
+    result = 0
+    if !block_given?
+
+      if self.is_a? Range
+        return self
+      end
+
       if arg.length == 1
-          return "Arg[0] is not a Symbol..." unless arg[0].kind_of? Symbol # Check if it's only a symbol 
-          our_sym=arg[0].to_s[0]
-          result = self[0]
-          i=1
-          while i<self.length
-              result=result.send(our_sym,self[i])
-              i+=1
-          end 
-          return result
+        return 'Arg[0] is not a Symbol...' unless arg[0].is_a? Symbol # Check if it's only a symbol
+
+        our_sym = arg[0].to_s[0]
+        result = self[0]
+        i = 1
+        while i < self.length
+          result = result.send(our_sym, self[i])
+          i += 1
+        end
+        return result
       end
       if arg.length == 2
-          our_sym = arg[1].to_s[0]
-          result = arg[0]
-          i=0
-          while i<self.length
-            result=result.send(our_sym,self[i])
-              i+=1
+        our_sym = arg[1].to_s[0]
+        result = arg[0]
+        i = 0
+        while i < self.length
+          result = result.send(our_sym, self[i])
+          i += 1
+        end
+        return result
+      end
+    elsif block_given?
+      if (arg.length == 1) && (arg[0].is_a? Integer)
+        if self.is_a? Range
+          result = self.to_a
+          acc = result[0]
+          i = 1
+          while i < result.length
+            puts "(Before yield): i is: #{i}, result is: #{result}, result[0] is: #{result[0]}, acc is: #{acc}"
+            acc = yield(acc, result[i])
+            i += 1
+          end
+          return acc
+        end
+        result = arg[0]
+        self.my_each { |el| result = yield(result, el) }
+        return result
+      end
+
+      if arg.length.zero?
+        if self.is_a? Range
+          result = self.to_a
+          acc = result[0]
+          i = 1
+          while i < result.length
+            puts "(Before yield): i is: #{i}, result is: #{result}, result[0] is: #{result[0]}, acc is: #{acc}"
+            acc = yield(acc, result[i])
+            i += 1
+          end
+          return acc
+        elsif self.is_a? Array
+          result = self[0]
+          i = 1
+          while i < self.length
+            result = yield(result, self[i])
+            i += 1
           end
           return result
+        end
       end
-  elsif block_given?
-    if (arg.length == 1) && (arg[0].kind_of? Integer)
-      result = arg[0]
-      self.my_each { |el| result=yield(result,el) }
-      return result
-    end
-    if arg.length.zero?
-      result = self[0]
-      i=1
-      while i<self.length
-          result= yield(result, self[i])
-          i+=1
-      end
-      return result
     end
   end
-end
-
 end
 
 def multiply_els(arg)
-  return arg.my_inject {|acc,el| acc*=el}
+  return arg.my_inject { |acc, el| acc * el }
 end
-
-puts [1,2,3,4].inject(:%)
-puts [1,2,3,4].my_inject(:%)
-puts [1,2,3,4].inject(5, :/)
-puts [1,2,3,4].my_inject(5, :/)
-#puts [1,2,3,4].inject(5, 3, :/)
-#puts "Testing my_inject with three arguments..."
-#puts [1,2,3,4].my_inject(5, 3, :/)
-#puts [1,2,3,4].my_inject()
-#puts [1,2,3,4].inject {|acc, elt| acc-= elt }
-#puts [1,2,3,4].my_inject { |acc, elt| acc-= elt } 
-#puts ["a", "b", "c", "d"].inject {|acc, elt| acc+= elt }
-#puts ["a", "b", "c", "d"].my_inject { |acc, elt| acc+= elt } 
-#puts "Testing my inject with a block and a single argument..."
-#puts [1,2,3,4].inject([1]) { |acc,el| acc%=el }
-#puts [1,2,3,4].my_inject([1]) { |acc,el| acc%=el }
--- tests.rb
+++ tests.rb
@@ -1,107 +1,108 @@
 require_relative 'enumerables.rb'
 
-# puts "======= Tests for my_each ======"
-# puts [3,5,67,23,4].my_each {|el,el2| puts el+el2}
+ puts "======= Tests for my_each ======"
+ puts "Built-in each: "
+ [ "a", "b", "c" ].each { |x| print x, " -- " }
+ puts ""
+ puts "my_each: "
+ [ "a", "b", "c" ].my_each { |x| print x, " -- " }
+ puts ""
 # puts "Built-in each: "
-# [ "a", "b", "c" ].each {|x| print x, " -- " }
-# puts ""
-# puts "my_each: "
-# [ "a", "b", "c" ].my_each {|x| print x, " -- " }
-# puts ""
-# puts [3,5,67,23,4].each {|el,el2| puts el+el2} # Fails, nil can't be coerced into Integer
-# puts [3,5,67,23,4].my_each {|el,el2| puts el+el2} # Fails, nil can't be coerced into Integer (same as each)
+# puts [3, 5, 67, 23, 4].each {|el,el2| puts el+el2} # Fails, nil can't be coerced into Integer
+# puts "Our my_each: "
+# puts [3, 5, 67, 23, 4].my_each {|el,el2| puts el+el2} # Fails, nil can't be coerced into Integer (same as each)
 
-#  puts "======= Tests for my_each_with_index ======"
-#  puts "It should return the enumerable if no block given"
-#  puts "Built-in each_with_index..."
-#  puts [1,2,3,4].each_with_index
-#  puts "Our my_each_with_index..."
-#  puts [1,2,3,4].my_each_with_index
-#  puts "It should execute the code given in the block"
-#  puts "Built-in each_with_index..."
-#  puts [1,2,3,4].each_with_index { |val, index| val * index }
-#  puts "Our my_each_with_index..."
-#  puts [1,2,3,4].my_each_with_index { |val, index| val * index }
-#  puts "Now, with a hash..."
-#  puts "Built-in each_with_index"
-#  hash = Hash.new
-# puts %w(cat dog wombat).each_with_index { |item, index|
-#   hash[item] = index
-# }
-# puts hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
-#  puts "Our my_each_with_index"
-#  hash = Hash.new
-#  puts %w(cat dog wombat).my_each_with_index { |item, index|
-#    hash[item] = index
-#  }
-#  puts hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
+  puts "======= Tests for my_each_with_index ======"
+  puts "It should return the enumerable if no block given"
+  puts "Built-in each_with_index..."
+  puts [1, 2, 3, 4].each_with_index
+  puts "Our my_each_with_index..."
+  puts [1, 2, 3, 4].my_each_with_index
+  puts "It should execute the code given in the block"
+  puts "Built-in each_with_index..."
+  puts [1, 2, 3, 4].each_with_index { |val, index| val * index }
+  puts "Our my_each_with_index..."
+  puts [1, 2, 3, 4].my_each_with_index { |val, index| val * index }
+  puts "Now, with a hash..."
+  puts "Built-in each_with_index"
+  hash = Hash.new
+ puts %w[cat dog wombat].each_with_index { |item, index|
+   hash[item] = index }
+# 
+ puts hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
+  puts "Our my_each_with_index"
+  hash = Hash.new
+  puts %w[cat dog wombat].my_each_with_index { |item, index|
+    hash[item] = index
+  }
+  puts hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
 
-# puts "======= Tests for my_select ======"
-#  puts "It should return the enumerable if no block given"
-#  puts "Built-in select"
-#  puts [1,2,3,4].select
-#  puts "Our my_select"
-#  puts [1,2,3,4].my_select
-#  puts "It should execute the code given in the block"
-#  puts "Built-in select"
-#  puts [1,2,3,4].select { |elt| elt > 2 }
-#  puts "Our my_select"
-#  puts [1,2,3,4].my_select { |elt| elt > 2 }
-#  puts "More tests: It should select the elements that satisfy the condition in the block"
-#  puts "Built-in select"
-#  puts (1..10).select { |i|  i % 3 == 0 }   #=> [3, 6, 9]
-#  puts [1,2,3,4,5].select { |num|  num.even?  }   #=> [2, 4]
-#  puts [:foo, :bar].select{ |x| x == :foo }   #=> [:foo]
-#  puts "Our my_select"
-#  puts (1..10).my_select { |i|  i % 3 == 0 }   #=> [3, 6, 9]
-#  puts [1,2,3,4,5].my_select { |num|  num.even?  }   #=> [2, 4]
-#  puts [:foo, :bar].my_select{ |x| x == :foo }   #=> [:foo]
+ puts "======= Tests for my_select ======"
+  puts "It should return the enumerable if no block given"
+  puts "Built-in select"
+  puts [1, 2, 3, 4].select
+  puts "Our my_select"
+  puts [1, 2, 3, 4].my_select
+  puts "It should execute the code given in the block"
+  puts "Built-in select"
+  puts [1, 2, 3, 4].select { |elt| elt > 2 }
+  puts "Our my_select"
+  puts [1, 2, 3, 4].my_select { |elt| elt > 2 }
+  puts "More tests: It should select the elements that satisfy the condition in the block"
+  puts "Built-in select"
+  puts (1..10).select { |i|  i % 3 == 0 }   #=> [3, 6, 9]
+  puts [1, 2, 3, 4, 5].select { |num|  num.even?  }   #=> [2, 4]
+  puts [:foo, :bar].select{ |x| x == :foo }   #=> [:foo]
+  puts "Our my_select"
+  puts (1..10).my_select { |i|  i % 3 == 0 }   #=> [3, 6, 9]
+  puts [1, 2, 3, 4, 5].my_select { |num|  num.even?  }   #=> [2, 4]
+  puts [:foo, :bar].my_select{ |x| x == :foo }   #=> [:foo]
 
 # puts "======= Tests for my_all ======"
-# puts "Built-in all?"
-# puts [nil, true, 99].all?
-# puts "Our my_all?"
-# puts [nil, true, 99].my_all?
-# puts "Built-in all?"
-# puts [1, 4, 5, 99, "g"].all? (Numeric)
-# puts "Our my_all?"
-# puts [1, 4, 5, 99, "g"].my_all? (Numeric)
-# puts "Built-in all?"
-# puts [8, 4, 4, 2].all? {|elt| elt % 2 == 0}
-# puts "Our my_all?"
-# puts [8, 4, 4, 2].my_all? {|elt| elt % 2 == 0}
-# puts "Built-in all?"
-# puts %w[ant bear cat].all? { |word| word.length >= 3 } #=> true
-# puts %w[ant bear cat].all? { |word| word.length >= 4 } #=> false
-# puts %w[ant bear cat].all?(/t/)                        #=> false
-# puts [1, 2i, 3.14].all?(Numeric)                       #=> true
-# puts [nil, true, 99].all?                              #=> false
-# puts [].all?                                           #=> true
-# puts "Our my_all?"
-# puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
-# puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
-# puts %w[ant bear cat].my_all?(/t/)                        #=> false
-# puts [1, 2i, 3.14].my_all?(Numeric)                       #=> true
-# puts [nil, true, 99].my_all?                              #=> false
-# puts [].my_all?                                           #=> true
+ puts "Built-in all?"
+ puts [nil, true, 99].all?
+ puts "Our my_all?"
+ puts [nil, true, 99].my_all?
+ puts "Built-in all?"
+ puts [1, 4, 5, 99, "g"].all? (Numeric)
+ puts "Our my_all?"
+ puts [1, 4, 5, 99, "g"].my_all? (Numeric)
+ puts "Built-in all?"
+ puts [8, 4, 4, 2].all? {|elt| elt % 2 == 0}
+ puts "Our my_all?"
+ puts [8, 4, 4, 2].my_all? {|elt| elt % 2 == 0}
+ puts "Built-in all?"
+ puts %w[ant bear cat].all? { |word| word.length >= 3 } #=> true
+ puts %w[ant bear cat].all? { |word| word.length >= 4 } #=> false
+ puts %w[ant bear cat].all?(/t/)                        #=> false
+ puts [1, 2i, 3.14].all?(Numeric)                       #=> true
+ puts [nil, true, 99].all?                              #=> false
+ puts [].all?                                           #=> true
+ puts "Our my_all?"
+ puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+ puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+ puts %w[ant bear cat].my_all?(/t/)                        #=> false
+ puts [1, 2i, 3.14].my_all?(Numeric)                       #=> true
+ puts [nil, true, 99].my_all?                              #=> false
+ puts [].my_all?                                           #=> true
 # puts "Built-in all?"
 # puts [1, 4, 5, 99, "g"].all? (Numeric, String) #They throw an error
 # puts "Our my_all?"
 # puts [1, 4, 5, 99, "g"].my_all? (Numeric, String) #They throw an error
 # puts "Built-in all?"
-# puts "all? is: #{[5,3,6].all?(Block)}" #They throw a NameError
+# puts "all? is: #{[5, 3, 6].all?(Block)}" #They throw a NameError
 # puts "Our my_all?"
-# puts "all? is: #{[5,3,6].my_all?(Block)}" #They throw a NameError
+# puts "all? is: #{[5, 3, 6].my_all?(Block)}" #They throw a NameError
 
 # puts "======= Tests for my_any ======"
 # puts "Built-in any?"
 # puts "any? is: #{[5,3,77].any?(Numeric)}"
 # puts "Our my_any?"
-# puts "my_any? is: #{[5,3,77].my_any?(Numeric)}"
+# puts "my_any? is: #{[5, 3, 77].my_any?(Numeric)}"
 # puts "Built-in any?"
-# puts "any? is: #{[5,3,77,"f"].any?(String)}"
+# puts "any? is: #{[5, 3, 77, "f"].any?(String)}"
 # puts "Our my_any?"
-# puts "my_any? is: #{[5,3,77,"f"].my_any?(String)}"
+# puts "my_any? is: #{[5, 3, 77, "f"].my_any?(String)}"
 # puts "Built-in any?"
 # puts [7, 5, 1, 5].any? {|elt| elt % 2 == 0}
 # puts "Our my_any?"
@@ -124,13 +125,13 @@ require_relative 'enumerables.rb'
 # puts "======= Tests for my_any ======"
 
 # puts "Built-in none?"
-# puts "none? is: #{[5,3,77].none?(Numeric)}"
+# puts "none? is: #{[5, 3, 77].none?(Numeric)}"
 # puts "Our my_none?"
-# puts "my_none? is: #{[5,3,77].my_none?(Numeric)}"
+# puts "my_none? is: #{[5, 3, 77].my_none?(Numeric)}"
 # puts "Built-in none?"
-# puts "none? is: #{[5,3,77,"f"].none?(String)}"
+# puts "none? is: #{[5, 3, 77, "f"].none?(String)}"
 # puts "Our my_none?"
-# puts "my_none? is: #{[5,3,77,"f"].my_none?(String)}"
+# puts "my_none? is: #{[5, 3, 77, "f"].my_none?(String)}"
 # puts "Built-in none?"
 # puts [7, 5, 1, 5].none? {|elt| elt % 2 == 0}
 # puts "Our my_none?"
@@ -157,13 +158,13 @@ require_relative 'enumerables.rb'
 
 #  puts "======= Tests for my_count ======"
 #  puts "Built-in count"
-#  puts [5,3,77].count(3)
+#  puts [5, 3, 77].count(3)
 #  puts "Our count"
-#  puts [5,3,77].my_count(3)
+#  puts [5, 3, 77].my_count(3)
 #  puts "Built-in count"
-#  puts [5,3,77,1].count { |elt| elt > 1 }
+#  puts [5, 3, 77, 1].count { |elt| elt > 1 }
 #  puts "Our count"
-#  puts [5,3,77,1].my_count { |elt| elt > 1 }
+#  puts [5, 3, 77, 1].my_count { |elt| elt > 1 }
 #  puts "Built-in count"
 #  puts [7, 5, 1, 5].count {|elt| elt.kind_of? Numeric}
 #  puts "Our count"
@@ -185,9 +186,9 @@ require_relative 'enumerables.rb'
 #  puts (1..4).my_map { |i| i*i }
 #  puts "When we don't pass anything"
 #  puts "Built-in map"
-#  puts [5,3,77,1].map
+#  puts [5, 3, 77, 1].map
 #  puts "Our my_map"
-#  puts [5,3,77,1].my_map
+#  puts [5, 3, 77, 1].my_map
 # puts "When we pass a Range within an Array and a block" # They fail the same way
 # puts "Built-in map"
 # puts [1..4].map { |i| i*i }
@@ -195,15 +196,15 @@ require_relative 'enumerables.rb'
 # puts [1..4].my_map { |i| i*i }
 # puts "When we pass an Array and a Block"
 # puts "Built-in map"
-# puts [1,2,3,4].map { |i| i*i }
+# puts [1, 2, 3, 4].map { |i| i*i }
 # puts "Our my_map"
-# puts [1,2,3,4].my_map { |i| i*i }
-# puts "With strings..." #They fail the same way
+# puts [1, 2, 3, 4].my_map { |i| i*i }
+# puts "With strings..." # They fail the same way
 # puts "Built-in map"
 # puts "something".map { |i| i*i }
 # puts "Our my_map"
 # puts "something".my_map { |i| i*i }
-# puts "With a string and no arguments" #They fail the same way
+# puts "With a string and no arguments" # They fail the same way
 # puts "Built-in map"
 # puts "Hello".map
 # puts "Our my_map"
@@ -211,9 +212,9 @@ require_relative 'enumerables.rb'
 # puts "Now, with a proc..."
 # my_proc = Proc.new {|el| el/=2}
 # puts "Built-in map"
-# puts [1,3,5].map(&my_proc)
+# puts [1, 3, 5].map(&my_proc)
 # puts "Our my_map"
-# puts [1,3,5].my_map(&my_proc)
+# puts [1, 3, 5].my_map(&my_proc)
 # puts "More tests..."
 # puts "Built-in map"
 # puts (1..4).map { |i| i*i }      #=> [1, 4, 9, 16]
@@ -225,51 +226,51 @@ require_relative 'enumerables.rb'
 puts "======= Testing my_inject==========="
 
 puts "Built-in inject"
-puts [1,2,3,4].inject { |acc, elt| acc * elt}
+puts [1, 2, 3, 4].inject { |acc, elt| acc * elt}
 puts "Our my_inject"
-puts [1,2,3,4].my_inject { |acc, elt| acc * elt}
+puts [1, 2, 3, 4].my_inject { |acc, elt| acc * elt}
 puts "Built-in inject"
-puts [1,2,3,4].inject(5) { |acc, elt| acc * elt}
+puts [1, 2, 3, 4].inject(5) { |acc, elt| acc * elt}
 puts "Our my_inject"
-puts [1,2,3,4].my_inject(5) { |acc, elt| acc * elt}
+puts [1, 2, 3, 4].my_inject(5) { |acc, elt| acc * elt}
 puts "Built-in inject"
-puts [1,2,3,4].inject(5, :+)
+puts [1, 2, 3, 4].inject(5, :+)
 puts "Our my_inject"
-puts puts [1,2,3,4].my_inject(5, :+)
+puts puts [1, 2, 3, 4].my_inject(5, :+)
 puts "Built-in inject"
-puts [1,2,3,4].inject(:+)
+puts [1, 2, 3, 4].inject(:+)
 puts "Our my_inject"
-puts [1,2,3,4].my_inject(:+)
+puts [1, 2, 3, 4].my_inject(:+)
 puts "Built-in inject"
-# puts ["a", "b", "c"].inject("d", :*) # RETURN TypeError
+# puts ["a", "b", "c"].inject("d", :*) # Returns TypeError
 puts "Our my_inject"
-# puts ["a", "b", "c"].my_inject("d", :*) # RETURN TypeError
+# puts ["a", "b", "c"].my_inject("d", :*) # Returns TypeError
 puts "Built-in inject"
-puts [1,2,4].inject([3,4], :*) #NOT SOLVED YET (returns the argument array the number of times equal to the reduce value)
+puts [1, 2, 4].inject([3,4], :*) # Returns the argument array the number of times equal to the reduced value)
 puts "Our my_inject"
-puts [1,2,4].my_inject([3,4], :*)
+puts [1, 2, 4].my_inject([3,4], :*)
 puts "Built-in inject"
-#puts {score1: 5, score2: 3, score3: 10}.inject { |elt| elt + 5 }
+# puts {score1: 5, score2: 3, score3: 10}.inject { |elt| elt + 5 }
 puts "Our my_inject"
-#puts {score1: 5, score2: 3, score3: 10}.my_inject { |elt| elt + 5 }
+# puts {score1: 5, score2: 3, score3: 10}.my_inject { |elt| elt + 5 }
 puts "Built-in inject"
-puts ["a", "b", "c", "d"].inject {|acc, elt| acc+= elt }
+puts ["a", "b", "c", "d"].inject { |acc, elt| acc += elt }
 puts "Our my_inject"
-puts ["a", "b", "c", "d"].my_inject { |acc, elt| acc+= elt }
+puts ["a", "b", "c", "d"].my_inject { |acc, elt| acc += elt }
 puts "Built-in inject"
-puts (5..10).inject { |sum, n| sum + n }            #=> 45
+# puts (5..10).inject { |sum, n| sum + n }            #=> 45
 puts (5..10).inject(1) { |product, n| product * n } #=> 151200
 longest = %w{ cat sheep bear }.inject do |memo, word|
    memo.length > word.length ? memo : word
 end
 puts longest                                        #=> "sheep"
 puts "Our my_inject"
-puts (5..10).my_inject { |sum, n| sum + n }            #=> 45
+# puts (5..10).my_inject { |sum, n| sum + n }            #=> 45
 puts (5..10).my_inject(1) { |product, n| product * n } #=> 151200
 longest = %w{ cat sheep bear }.inject do |memo, word|
    memo.length > word.length ? memo : word
 end
-puts longest                                        #=> "sheep"
+puts longest #=> "sheep"
 # puts "Built-in inject"
 # puts "line".inject("simple") { |acc, elt| acc * elt} " #Returns a NoMethodError
 # puts "Our my_inject"
@@ -287,17 +288,17 @@ puts longest                                        #=> "sheep"
 # puts "Our my_inject"
 # puts "line".my_inject(:+)" #Returns a NoMethodError
 # puts "Built-in inject"
-# puts [1,2,3,4].inject (Return Local JumpError no block given)
+# puts [1, 2, 3, 4].inject (Returns Local JumpError no block given)
 # puts "Our my_inject"
-# puts [1,2,3,4].my_inject (Return Local JumpError no block given)
+# puts [1, 2, 3, 4].my_inject (Returns Local JumpError no block given)
 # puts "Built-in inject"
-# puts [1,2,3,4].inject(4) (Return TypeError)
+# puts [1, 2, 3, 4].inject(4) (Returns TypeError)
 # puts "Our my_inject"
-# puts [1,2,3,4].my_inject(4) (Return TypeError)
+# puts [1, 2, 3, 4].my_inject(4) (Returns TypeError)
 # puts "Built-in inject"
-# puts [1,2,3,4].inject([3,4,5,6]) (Return a TypeError)
+# puts [1, 2, 3, 4].inject([3, 4, 5, 6]) (Returns a TypeError)
 # puts "Our my_inject"
-# puts [1,2,3,4].inject([3,4,5,6]) (Return a TypeError) 
+# puts [1, 2, 3, 4].inject([3, 4, 5, 6]) (Returns a TypeError) 
 
 # puts "Testing with multiply_els..."
-# puts multiply_els([2,4,7,5])
+# puts multiply_els([2, 4, 7, 5])
